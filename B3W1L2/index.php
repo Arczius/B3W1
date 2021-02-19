c90f2042b6d3a4b4673a1af139cdcf100678fbb6
Damian Vaartmans <99061149@mydavinci.nl>
2021-02-19 12:02:38 +0100

    verder gewerkt

--- B3W1L2/index.php
+++ B3W1L2/index.php
@@ -6,10 +6,21 @@
 <body>
 <?php
 	echo '<h1>'."hello world!".'</h1>';
+	
+	define("helloWorld", '<h1>'."hello world!".'</h1>'); 
+	echo helloWorld; 
+
+	$hellowWorld = '<h1>'."Learning PHP".'</h1>';
+	echo $hellowWorld;
+	$hellowWorld = '<h1>'."hello world!".'</h1>';
+	echo $hellowWorld;
 ?>
-<?php
-define("helloWorld", '<h1>'."hello world!".'</h1>'); 
-echo helloWorld;
-?>
+<h1>
+	<?php
+		$hello = "hello";
+		$world = " world!";
+		echo $hello.$world;
+	?>
+</h1>
 </body>
 </html>
